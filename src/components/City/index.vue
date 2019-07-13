54e13c6d11eb0d63fdfb619bc2d1c9cbd1c48f7b
Olivia <2393844957@qq.com>
2019-07-13 10:38:04 +0800

    bug clear

--- src/components/City/index.vue
+++ src/components/City/index.vue
@@ -1,11 +1,36 @@
 <template>
     <div class="city_body">
-
-   <div class="city_list">
-        
-          <Loading v-if="isLoading" />
-        <Scroller v-else>
-              <div>
+        <!-- <div class="city_list">
+            <div class="city_hot">
+                <h2>热门城市</h2>
+                <ul class="clearfix">
+                    <li>上海</li>
+                </ul>
+            </div>
+            <div class="city_sort">
+                <div>
+                    <h2>A</h2>
+                    <ul>
+                        <li>阿拉善盟</li>
+                        <li>鞍山</li>
+                        <li>安庆</li>
+                        <li>安阳</li>
+                    </ul>
+                </div>	
+            </div>
+        </div>
+        <div class="city_index">
+            <ul>
+                <li>A</li>
+                <li>B</li>
+                <li>C</li>
+                <li>D</li>
+                <li>E</li>
+            </ul>
+        </div> -->
+        <div class="city_list">
+            <Loading v-if="isLoading" />
+            <Scroller v-else ref="city_List">
                 <div>
                     <div class="city_hot">
                         <h2>热门城市</h2>
@@ -22,28 +47,24 @@
                         </div>	
                     </div>
                 </div>
-                </div>
             </Scroller>
         </div>
         <div class="city_index">
-            
             <ul>
                 <li v-for="(item,index) in cityList" :key="item.index" @touchstart="handleToIndex(index)">{{ item.index }}</li>
             </ul>
-             
         </div>
-   
-	</div>
-		
+    </div>
 </template>
+
 <script>
-export default{
-    name:'City',
+export default {
+    name : 'City',
     data(){
-        return{
-            cityList:[],
-             isLoading : true,
-            hotList:[]
+        return {
+            cityList : [],
+            hotList : [],
+            isLoading : true
         }
     },
     mounted(){
@@ -72,8 +93,8 @@ export default{
             });
         }
     },
-    methods:{
-            formatCityList(cities){
+    methods : {
+        formatCityList(cities){
             var cityList = [];
             var hotList = [];
 
@@ -140,7 +161,14 @@ export default{
     }
 }
 </script>
+
 <style scoped>
+#content .city_body{ margin-top: 45px; display: flex; width:100%; position: absolute; top: 0; bottom: 0;}
+.city_body .city_list{ flex:1; overflow: auto; background: #FFF5F0;}
+.city_body .city_list::-webkit-scrollbar{
+    background-color:transparent;
+    width:0;
+}
 .city_body .city_hot{ margin-top: 20px;}
 .city_body .city_hot h2{ padding-left: 15px; line-height: 30px; font-size: 14px; background:#F0F0F0; font-weight: normal;}
 .city_body .city_hot ul li{ float: left; background: #fff; width: 29%; height: 33px; margin-top: 15px; margin-left: 3%; padding: 0 4px; border: 1px solid #e6e6e6; border-radius: 3px; line-height: 33px; text-align: center; box-sizing: border-box;}
--- src/components/Scroller/index.vue
+++ src/components/Scroller/index.vue
@@ -8,34 +8,33 @@
 import BScroll from 'better-scroll';
 export default {
     name : 'Scroller',
-    props:{
-        handleToScroll:{
-            type:Function,
-            default:function(){}
+    props : {
+        handleToScroll : {
+            type : Function,
+            default : function(){}
         },
-        handleToTouchEnd:{
-            type:Function,
-            default:function(){}
+        handleToTouchEnd : {
+            type : Function,
+            default : function(){}
         }
     },
-    
     mounted(){
         var scroll = new BScroll( this.$refs.wrapper , {
             tap : true,
             probeType: 1
         });
+
+        this.scroll = scroll;
+
         scroll.on('scroll',(pos)=>{
             this.handleToScroll(pos);
         });
+
         scroll.on('touchEnd',(pos)=>{
             this.handleToTouchEnd(pos);
         });
-         this.scroll = scroll;
-
- 
     },
-        methods : {
-
+    methods : {
         toScrollTop(y){
             this.scroll.scrollTo(0,y);
         }
